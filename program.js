f286d129a013a0691c506e79b3e68d8076159660
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-12 07:52:38 +0000

    Step 4

--- program.js
+++ program.js
@@ -1,8 +1,14 @@
 var fs = require("fs");
-var str = fs.readFileSync(process.argv[2]).toString();
-var arr = str.split("\n");
-if(arr.length) {
- console.log(arr.length-1);
-} else {
- console.log(0);
-}
+fs.readFile(process.argv[2],function(err, data) {
+	if(err) {
+		return console.error("Error Ocured while reading the file");
+	}
+	var str = data.toString();
+	var arr = str.split("\n");
+	if(arr.length) {
+	 console.log(arr.length-1);
+	} else {
+	 console.log(0);
+	}
+	
+});
