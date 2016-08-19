8659a02619309a5128f6db076ea12d2635ba3cf5
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-19 10:52:13 +0000

    Step 8

--- program.js
+++ program.js
@@ -1,14 +1,43 @@
 var http = require("http");
-var bl  = require("bl");
+var content = [];
+var count = 0;
 
 http.get(process.argv[2], function(response) {
-	response.pipe(bl(function (err, data) {
-		if(err) {
-			return console.error(err);
-		}
-		var str = data.toString();
-		console.log(str.length);
-		console.log(str);
-	}));
-	
-}).on("error", console.error);
+	var body = "";
+	response.on("data", function(data) {
+		body += data;
+	});
+	response.on("end", function() {
+		content[0] = body;
+		count++;
+	});
+});
+
+http.get(process.argv[3], function(response) {
+	var body = "";
+	response.on("data", function(data) {
+		body += data;
+	});
+	response.on("end", function() {
+		content[1] = body;
+		count++;
+	});
+});
+
+http.get(process.argv[4], function(response) {
+	var body = "";
+	response.on("data", function(data) {
+		body += data;
+	});
+	response.on("end", function() {
+		content[2] = body;
+		count++;
+	});
+});
+
+var refreshIntervalId = setInterval(function() {
+	if(count === 3) {
+		clearInterval(refreshIntervalId);
+		console.log(content.join("\n"));
+	}
+}, 100);
