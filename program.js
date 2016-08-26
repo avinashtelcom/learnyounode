34df7bac88a6a501cae870c88bc18955421fd417
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-26 10:47:03 +0000

    Step 9

--- program.js
+++ program.js
@@ -1,43 +1,9 @@
-var http = require("http");
-var content = [];
-var count = 0;
+var net = require("net");
+var strftime = require("strftime");
+var data = new Date();
+data = strftime("%Y-%m-%d %H:%M", data);
 
-http.get(process.argv[2], function(response) {
-	var body = "";
-	response.on("data", function(data) {
-		body += data;
-	});
-	response.on("end", function() {
-		content[0] = body;
-		count++;
-	});
+var server = net.createServer(function(socket) {
+    socket.end(data+"\n");
 });
-
-http.get(process.argv[3], function(response) {
-	var body = "";
-	response.on("data", function(data) {
-		body += data;
-	});
-	response.on("end", function() {
-		content[1] = body;
-		count++;
-	});
-});
-
-http.get(process.argv[4], function(response) {
-	var body = "";
-	response.on("data", function(data) {
-		body += data;
-	});
-	response.on("end", function() {
-		content[2] = body;
-		count++;
-	});
-});
-
-var refreshIntervalId = setInterval(function() {
-	if(count === 3) {
-		clearInterval(refreshIntervalId);
-		console.log(content.join("\n"));
-	}
-}, 100);
+server.listen(process.argv[2]);
