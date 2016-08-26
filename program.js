52e43d86949896cf09d570015893cd4cce9ab702
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-26 12:22:58 +0000

    Step 12

--- program.js
+++ program.js
@@ -1,15 +1,30 @@
 var http = require("http");
-var map = require('through2-map');
+var url = require("url");
+var strftime = require("strftime");
 
 var server = http.createServer(function(request, response) {
   
-  if (request.method != 'POST')  
-         return response.end('send me a POST\n');
+  if (request.method != 'GET')  
+    return response.end('send me a GET\n');
          
-  response.writeHead(200, { 'content-type': 'text/plain' });
-  request.pipe(map(function (chunk) {  
-       return chunk.toString().toUpperCase();
-     })).pipe(response);
-   
+  response.writeHead(200, { 'content-type': 'text/json' });
+  
+  var url1 = url.parse(request.url, true);
+  var iso = url1.query.iso;
+  var pathname = url1.pathname;
+  var date = new Date(iso);
+  if(pathname === '/api/parsetime') {
+      
+      response.end(JSON.stringify({  
+       "hour": date.getHours(),  
+       "minute": date.getMinutes(),  
+       "second": date.getSeconds()  
+     }));
+  }
+  if(pathname === '/api/unixtime') {
+      response.end(JSON.stringify({ "unixtime": date.getTime() }));
+  }
+  
+  
 });
 server.listen(process.argv[2]);
